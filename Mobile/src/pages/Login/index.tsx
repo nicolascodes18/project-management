3edfef1c67b4df0a825f88ef21d045f295c39a66
Nicolas Souza Gomes <nicolas.gomes@imagemseguranca.com.br>
2022-02-26 09:12:55 -0300

    Base configuration mobile

---
+++ Mobile/src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+import { Container } from './style';
+
+export function Login() {
+    return(
+        <Container>
+            <Text>Login</Text>
+        </Container>
+    )
+}
